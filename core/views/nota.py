9d33b548c601f735ee18924de2888822bbd613af
LuizNunes06 <Luiz.Nunes180106@gmail.com>
2024-11-30 06:35:57 -0300

    FEAT: Filtros em Notas, Observações e Ocorrencias

--- core/views/nota.py
+++ core/views/nota.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 from core.models import Nota
 from core.serializers import NotaSerializer
@@ -7,3 +9,5 @@ from core.serializers import NotaSerializer
 class NotaViewSet(ModelViewSet):
     queryset = Nota.objects.all()
     serializer_class = NotaSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["aluno__id", "disciplina__id"]
