e7d65199c553195bb6856107f55d2634b9de4d66
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-11-30 06:42:57 -0300

    CHHORE: Ações em Observações

--- core/models/observacao.py
+++ core/models/observacao.py
@@ -7,6 +7,14 @@ class Observacao(models.Model):
     descricao = models.TextField()
     aluno = models.ForeignKey(Aluno, on_delete=models.PROTECT)
     trimestre = models.ForeignKey(Trimestre, on_delete=models.PROTECT)
+    acoes = models.CharField(max_length=3,
+    choices=[
+        ('FAM', 'Reunião Família Escola'), 
+        ('GPE', 'Participação em Grupo de Estudo'),
+        ('RSI', 'Reunião SISAE'),
+        ('RCC', 'Reunião Coordenação de Curso')
+    ]                         
+    )
 
     class Meta:
         verbose_name = "Observação"
