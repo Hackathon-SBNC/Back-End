100351badb1dfcd4e20c4136f8465b862a46858f
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-11-30 06:43:39 -0300

    FIX: Correção rotas do token

--- app/urls.py
+++ app/urls.py
@@ -44,6 +44,6 @@ urlpatterns = [
     # API
     path("api/", include(router.urls)),
 
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
